c7d8971bbd639c12ae0ba55a14ebe91a8ab24296
cicdata <hieutm211@gmail.com>
2021-02-23 13:37:22 +0700

    remove webvital

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 function App(): JSX.Element {
-    return <div className="bg-black">abc</div>
+    return <div className="mt-10 flex justify-center text-2xl font-bold">Template</div>
 }
 
 export default App
